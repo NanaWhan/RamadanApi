e7726f89ad3b09e5624e1fdca25b525b06c29b78
Dev Seinty <cherish.seinty@hubtel.com>
2025-03-08 07:32:04 +0000

    bug fixes

---
+++ RamadanReliefAPI/Models/DomainModels/Partners.cs
@@ -0,0 +1,14 @@
+﻿namespace RamadanReliefAPI.Models.DomainModels;
+
+public class Partners
+{
+    public Guid Id { get; set; }
+    public string OrganizationName { get; set; }
+    public string ContactPerson { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string? City { get; set; }
+    public string Message { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public bool IsActive { get; set; } = true;
+}
