b728b80b0080014f76270348b8400325c0d99ddf
Dev Seinty <cherish.seinty@hubtel.com>
2025-03-14 04:19:09 +0000

    afaed

---
+++ RamadanReliefAPI/Actors/Messages/DonationFormSubmittedMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace RamadanReliefAPI.Actors.Messages;
+
+public class DonationFormSubmittedMessage
+{
+    public string FullName { get; }
+    public string Email { get; }
+    public string PhoneNumber { get; }
+    public decimal Amount { get; }
+    public string AdminPhoneNumber { get; }
+
+    public DonationFormSubmittedMessage(string fullName, string email, string phoneNumber, decimal amount, string adminPhoneNumber)
+    {
+        FullName = fullName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Amount = amount;
+        AdminPhoneNumber = adminPhoneNumber;
+    }
+}
---
+++ RamadanReliefAPI/Models/Dtos/Donation/DonationFormRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RamadanReliefAPI.Models.Dtos.Donation;
+
+public class DonationFormRequest
+{
+    [Required(ErrorMessage = "Full name is required")]
+    public string FullName { get; set; }
+    
+    [Required(ErrorMessage = "Email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Phone number is required")]
+    public string PhoneNumber { get; set; }
+    
+    [Required]
+    [Range(1, 100000, ErrorMessage = "Amount must be greater than 0")]
+    public decimal Amount { get; set; }
+}
